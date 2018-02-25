48c10fae4b4e75c3887a9c8dbf80f36ba626e829
tyoras <tyoras@gmail.com>
2018-02-25 18:05:28 +0100

    customize install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Creating symlink to your oh-my-zsh themes path...";
-ln -f agitnoster.zsh-theme ~/.oh-my-zsh/themes/agitnoster.zsh-theme;
+ln -f tyoras-agitnoster.zsh-theme ~/.oh-my-zsh/themes/tyoras-agitnoster.zsh-theme;
 
 echo "Configuring theme in .zshrc...";
 if [[ `grep -e '^ZSH_THEME' ~/.zshrc | wc -l` -ne "1" ]]; then
-    echo ZSH_THEME="agitnoster.zsh-theme" >> ~/.zshrc;
+    echo ZSH_THEME="tyoras-agitnoster.zsh-theme" >> ~/.zshrc;
 else
-    sed ~/.zshrc -i -e 's/ZSH_THEME="[.a-z0-9\-]*"/ZSH_THEME="agitnoster"/g';
+    sed ~/.zshrc -i -e 's/ZSH_THEME="[.a-z0-9\-]*"/ZSH_THEME="tyoras-agitnoster"/g';
 fi
